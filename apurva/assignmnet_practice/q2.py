d6f442b2bf1d2e32faf90baaca9de7d1045a0572
Apurva-chavan <apurvachavan306@email.com>
2026-02-11 21:58:58 +0530

    Q2 solutions

---
+++ apurva/assignmnet_practice/q2.py
@@ -0,0 +1,20 @@
+n = int(input("Enter number of records: "))
+
+credits = {}
+affected = set()
+
+for i in range(n):
+    print("Enter: employee_id time amount")
+    emp_id, time, amount = map(int, input().split())
+
+    if amount > 0:
+        credits.setdefault(emp_id, []).append((time, amount))
+    else:
+        if emp_id in credits:
+            for credit_time, credit_amount in credits[emp_id]:
+                if credit_amount == -amount and 0 <= time - credit_time <= 24:
+                    affected.add(emp_id)
+
+print("Affected employees:")
+for emp in sorted(affected):
+    print(emp)
